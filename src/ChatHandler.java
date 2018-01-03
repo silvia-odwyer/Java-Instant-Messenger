e003039ca34788527a97df7c793178ac2bc0cbd3
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-01-03 21:26:02 +0000

    Coded the run() method for the ChatHandler class.

--- src/ChatHandler.java
+++ src/ChatHandler.java
@@ -3,12 +3,14 @@ import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Vector;
 
 public class ChatHandler extends Thread {
-	protected DataInputStream inputStream;
-	protected DataOutputStream outputStream;
+	protected ObjectInputStream inputStream;
+	protected ObjectOutputStream outputStream;
 	protected Socket newClient;
 	protected static Vector handlers = new Vector();
 	
@@ -19,18 +21,44 @@ public class ChatHandler extends Thread {
 		
 	}
 	public void setUpStreams() throws IOException {
-		inputStream = new DataInputStream(new BufferedInputStream (newClient.getInputStream()));
-		outputStream = new DataOutputStream(new BufferedOutputStream (newClient.getOutputStream()));
+		inputStream = new ObjectInputStream(newClient.getInputStream());
+		outputStream = new ObjectOutputStream(new BufferedOutputStream (newClient.getOutputStream()));
+		outputStream.flush();
 	}
 	
 
 	public void run() {
 		try {
 			handlers.addElement (this);
-			whileChatting();
+			while (true) {
+				String message = inputStream.readUTF();
+				broadcast(message);
+			} catch(IOException ioException) {
+				ioException.printStackTrace();
+			} finally {
+				handlers.removeElement(this);
+				
+				try {
+					newClient.close();
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				}
+			}
 			
 		}
 		
+	public void whileChatting() {
+			
 	}
-
+	
+	public void close() throws IOException {
+		inputStream.close();
+		outputStream.close();
+		newClient.close();
+	}
+	
+	public void broadcast() {
+		
+	}
+		
 }
