a5dd4ee09780582d293d7b997439257b996a4ead
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-01-22 23:20:30 +0000

    I'm moving to the Java Spring framework, in order to maximise use of the Java Message Service (since Spring can optimize and abstractify the JMS), so all JavaFX files have been removed.

--- src/ServerTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-
-public class ServerTest {
-	public static void main(String[] args) {
-		Server silvia = new Server();
-		silvia.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		silvia.runApplication();
-	}
-}
