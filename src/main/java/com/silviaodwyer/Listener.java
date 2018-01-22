83eb5b779131b38f421639ecf6ee847024370919
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-01-22 23:34:04 +0000

    Created a Listener class, which listens for messages, takes in their contents, and displays them to the user.

---
+++ src/main/java/com/silviaodwyer/Listener.java
@@ -0,0 +1,14 @@
+package com.silviaodwyer;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Listener {
+	@JmsListener(destination = "mailbox", containerFactory = "aFactory")
+	public void receiveMessageContents(Message message) {
+		System.out.println("You have received a message.");
+		System.out.println("The message you received is: ");
+		System.out.println("MESSAGE: " + message + ".");
+	}
+}
