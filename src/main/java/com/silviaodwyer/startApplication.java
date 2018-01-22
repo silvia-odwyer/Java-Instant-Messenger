0df14a7d0143c3fffb8051277e1691897aabe7ae
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-01-22 23:46:15 +0000

    Created methods to launch the Spring application in the startApplication class.

---
+++ src/main/java/com/silviaodwyer/startApplication.java
@@ -0,0 +1,45 @@
+package com.silviaodwyer;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@SpringBootApplication
+@EnableJms
+public class startApplication {
+	
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+													DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		
+		configurer.configure(factory,  connectionFactory);
+		
+		return factory;
+	}
+	
+	@Bean // Converts all the messages to json
+	public MessageToJsonConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter jsonConverter = new MappingJackson2MessageConverter();
+		jsonConverter.setTargetType(MessageType.TEXT);
+		jsonConverter.setTypeIdPropertyName("_type");
+		return jsonConverter;
+	}
+	
+	public static void main(String[] args) {
+		// The following line will launch the application
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+	}
+	
+	
+
+}
